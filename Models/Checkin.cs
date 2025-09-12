283ca00a5a9b2987adf54b62e0e31f606caafd77
unknown <ricardo.andrade@ucb.edu.bo>
2025-09-12 16:47:44 -0400

    feat: creating create checkin Dto

--- Models/Checkin.cs
+++ Models/Checkin.cs
@@ -4,7 +4,15 @@ public class Checkin
 {
     public Guid Id { get; set; }
 
+    [Required, StringLength(100)]
     public string BadgeCode { get; set; } = string.Empty;
 
     public  DateTime Timestap{ get; set; } 
 }
+// first DTO
+public record CreateCheckInDto
+{
+    [Required, StringLength(100)]
+    public string BadgeCode { get; init; } = string.Empty;
+}
+
