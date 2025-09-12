fd5153ebc084203a029f3dc90b8b6e25941fe091
shalure10lure <yessicarado@gmail.com>
2025-09-12 16:30:42 -0400

    feat: adding DTO UpdateMemberDto to Models member

--- Models/Member.cs
+++ Models/Member.cs
@@ -1,6 +1,48 @@
-﻿namespace FirstExam.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+public class Member
 {
-    public class Member
-    {
-    }
+    public Guid Id { get; set; }
+
+    [Required, StringLength(100)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required, StringLength(100)]
+    public string FullName { get; set; } = string.Empty;
+
+    [Required]
+    public bool active { get; set; } 
+}
+/*
+  Guid Id,
+  string Email,
+  string FullName,
+  bool Active
+  */
+
+//DTOs
+//CreateMemberDto
+public record CreateMemberDto
+{
+    [Required, StringLength(100)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required, StringLength(100)]
+    public string FullName { get; set; } = string.Empty;
+
+    [Required]
+    public bool active { get; set; }
+}
+//UpdateMemberDto
+
+public record UpdateMemberDto
+{
+    [Required, StringLength(100)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required, StringLength(100)]
+    public string FullName { get; set; } = string.Empty;
+
+    [Required]
+    public bool active { get; set; }
 }
