caeee528366f1dc3081cbad8988cac037888314d
unknown <dabnerorozco123@gmail.com>
2025-09-12 16:37:07 -0400

    feat: creating membership model

---
+++ Models/Membership.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Membership
+{
+    public Guid Id { get; set; }
+
+    [Required]
+    public Guid MemberId { get; set; }
+
+    [StringLength(10)]
+    [RegularExpression("^(basic|pro|premium)$")]
+    public string Plan { get; set; } = string.Empty;
+   
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+
+    [StringLength(10)]
+    [RegularExpression("^(active|expired|canceled)$")]
+    public string Status { get; set; } = string.Empty;
+}
