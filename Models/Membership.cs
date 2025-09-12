4ae32892789c10468b223066980075b3a4a45428
Adrian Claros <166774927+AdrianCV2345@users.noreply.github.com>
2025-09-12 16:34:10 -0400

    feat: Membership model and the created.

---
+++ Models/Membership.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Membership
+{
+    public Guid id { get; init; }
+
+    public Guid MemberId { get; init; }
+
+    [Required, StringLength(100)]
+    public string plan { get; init; } = string.Empty;
+
+    [Required, StringLength(100)]
+    public DateTime StartTime { get; init; }
+
+    [Required, StringLength(100)]
+    public DateTime Endtime { get; init; }
+
+    [Required, StringLength(100)]
+    public string status { get; init; } = string.Empty;
+}
+
