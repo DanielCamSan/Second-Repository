1a35cbc78ceab1c2c2074291ab4eabedccc7cf69
Flora <Flora.ortiz@ucb.edu.bo>
2025-09-12 16:42:15 -0400

    feat: creating MembersController

---
+++ Controllers/MembersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace FirstExam.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MembersController : ControllerBase
+    {
+
+    }
+}
