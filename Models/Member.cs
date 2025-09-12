2c4a4935b39e13172b705e4b76fa98358ea1d9bb
Maria <mariajesuslozada10@gmail.com>
2025-09-12 16:49:53 -0400

    adding: DTOs for member

--- Models/Member.cs
+++ Models/Member.cs
@@ -14,4 +14,21 @@ public class Member
     public bool Active { get; set; } = true;
 
 }
+//DTOs for creating and updating members
 
+public class CreateMemberDto
+{
+    [Required, StringLength(100)]
+    public string Email { get; set; } = string.Empty;
+    [Required, StringLength(100)]
+    public string FullName { get; set; } = string.Empty;
+}
+
+public class UpdateMemberDto
+{
+    [Required, StringLength(100)]
+    public string Email { get; set; } = string.Empty;
+    [Required, StringLength(100)]
+    public string FullName { get; set; } = string.Empty;
+    public bool Active { get; set; } = true;
+}
