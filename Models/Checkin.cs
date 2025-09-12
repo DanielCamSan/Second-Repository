6cf2db0d95c3ffc53d1658271efe730c1a40a4f3
unknown <you@example.com>
2025-09-12 16:39:06 -0400

    create: new model for checkins

---
+++ Models/Checkin.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+public class Checkin
+{
+    public Guid Id { get; set; }
+
+    [Required, StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [Range(0, 4000)]
+    public int Hour { get; set; }
+}
+
