15c63485987a8591ddb330dd5c3faa0eb48226d8
unknown <ricardo.andrade@ucb.edu.bo>
2025-09-12 16:15:00 -0400

    feat: starting brach

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -6,6 +6,8 @@ namespace FirstExam.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
