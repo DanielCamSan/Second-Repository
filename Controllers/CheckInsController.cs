113262fdf24ab91cdd4f5497ef66c6522c521edb
Rodrigo <rovillaaya10@gmail.com>
2025-09-12 16:43:13 -0400

    Feat: Model of CheckIns ready

---
+++ Controllers/CheckInsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System.Runtime.Intrinsics.X86;
+
+
+namespace FirstExam.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CheckInsController: ControllerBase
+    {
+
+    }
+}
---
+++ Models/CheckIns.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstExam.Models
+{
+    public class CheckIns
+    {
+        public Guid Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string BadgeCode { get; set; }= string.Empty;
+        DateTime Timestamp { get; set; } = DateTime.Now;
+
+
+
+    }
+        
+}
+
+public record CreateCheckInsDto
+{
+    [Required, StringLength(100)]
+    public string BadgeCode { get; set; } = string.Empty;
+
+    [Required, StringLength(100)]
+    public DateTime Timestamp { get; set; } = DateTime.Now;
+
+}
+
+public record UpdateCheckInsDto
+{
+    [Required, StringLength(100)]
+    public string BadgeCode { get; set; } = string.Empty;
+
+    [Required, StringLength(100)]
+    public DateTime Timestamp { get; set; } = DateTime.Now;
+
+}
