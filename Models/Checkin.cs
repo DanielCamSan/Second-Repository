bca5d1dd5ea9b49b6c7f38029478815d02beb446
unknown <you@example.com>
2025-09-12 16:46:33 -0400

    feat: adding create and update DTOs

--- Models/Checkin.cs
+++ Models/Checkin.cs
@@ -9,4 +9,21 @@ public class Checkin
     [Range(0, 4000)]
     public DateTime Timestamp { get; set; }
 }
+public record CreateCheckinDto
+{
+    [Required, StringLength(100)]
+    public string BadgeCode { get; set; } = string.Empty;
+
+    [Range(0, 4000)]
+    public DateTime Timestamp { get; set; }
+}
+
+public record UpdateCheckinDto
+{
+    [Required, StringLength(100)]
+    public string BadgeCode { get; set; } = string.Empty;
+
+    [Range(0, 4000)]
+    public DateTime Timestamp { get; set; }
+}
 
