a25b3f04dfae29dd4c82dfedb072c411c6e1c1f7
unknown <ricardo.andrade@ucb.edu.bo>
2025-09-12 16:40:28 -0400

    feat: creating model checkin.cs

---
+++ Models/Checkin.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+public class Checkin
+{
+    public Guid Id { get; set; }
+
+    public string BadgeCode { get; set; } = string.Empty;
+
+    public  DateTime Timestap{ get; set; } 
+}
