a7410417f0ef445dc7573a10030ecc179179a6eb
Soria <rene.soria@ucb.edu.bo>
2025-09-12 16:55:40 -0400

    feat: adding class Member

---
+++ Models/Member.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+    public class Member
+    {
+    public Guid Id { get; set; }
+    [Required,StringLength(200)]
+    public string Email {  get; set; }=string.Empty;
+    [Required, StringLength(200)]
+    public string FullName {  get; set; }=string.Empty;
+    [Required]
+    public bool Active { get; set; }
+
+}
+
+
+
