bca94e7e15b5bee591713b1ed495c54b8408430d
isa2206 <hidalgoavilaisabel@gmail.com>
2025-09-12 17:12:57 -0400

    feat: add MembersContorller and Model of Member

---
+++ Models/Member.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+
+
+public class Member
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
+    public bool Active { get; set; }
+}
+
+public class CreateMemberDto
+{
+    [Required, EmailAddress, StringLength(150)]
+    public string Email { get; init; } = string.Empty;
+
+    [Required, StringLength(150)]
+    public string FullName { get; init; } = string.Empty;
+
+    public bool Active { get; init; }
+}
+
+public class UpdateMemberDto
+{
+    [Required, EmailAddress, StringLength(150)]
+    public string Email { get; init; } = string.Empty;
+
+    [Required, StringLength(150)]
+    public string FullName { get; init; } = string.Empty;
+
+    public bool Active { get; init; }
+}
+
