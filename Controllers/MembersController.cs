905ebc52cab999508b1313c3ba294a326e4642fb
Flora <Flora.ortiz@ucb.edu.bo>
2025-09-12 16:52:37 -0400

    feat: mock list

--- Controllers/MembersController.cs
+++ Controllers/MembersController.cs
@@ -9,8 +9,10 @@ namespace FirstExam.Controllers
     {
         private static readonly List<Member> _members = new()
         {
-            new Member { Id = Guid.NewGuid(), Email = "j.deer@gmail.com" , FullName = "John Deer", Active = true}
-
+            new Member { Id = Guid.NewGuid(), Email = "j.deer@gmail.com" , FullName = "John Deer", Active = true},
+            new Member { Id = Guid.NewGuid(), Email = "jane.doe@gmail.com" , FullName = "Jane Doe", Active = true},
+            new Member { Id = Guid.NewGuid(), Email = "andy.andrade@gmail.com" , FullName = "Andres Andrade", Active = true},
+            new Member { Id = Guid.NewGuid(), Email = "dahbner@gmail.com" , FullName = "Dabner Orozco", Active = false}
         };
     }
 }
