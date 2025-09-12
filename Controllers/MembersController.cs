48b2c1771a2357ee50b2a43dde9ddaaa47053c15
Maria <mariajesuslozada10@gmail.com>
2025-09-12 16:31:14 -0400

    adding: membersController

---
+++ Controllers/MembersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace FirstExam.Controllers
+{
+    public class MembersController
+    {
+        [ApiController]
+        [Route("api/[controller]")]
+
+        public class MembersController : ControllerBase
+        {
+            private static List<Member> members = new List<Member>
+            {
+                new Member { Id = 1, Name = "John Doe", Email = ""}
+
+            };
+
+
+        
+        
+        }
+}
