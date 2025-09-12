312280f38327531c8ea8d3b481b70454dc8d0dac
shalure10lure <yessicarado@gmail.com>
2025-09-12 17:22:09 -0400

    feat: adding api/v1

--- Controllers/MembersController.cs
+++ Controllers/MembersController.cs
@@ -23,12 +23,11 @@ namespace FirstExam.Controllers
             var l = limit.GetValueOrDefault(10); if (l < 1) l = 1; if (l > 100) l = 100;
             return (p, l);
         }
-        //ordenamiento dinamico
         private static IEnumerable<T> OrderByProp<T>(IEnumerable<T> src, string? sort, string? order)
         {
-            if (string.IsNullOrWhiteSpace(sort)) return src; // no-op
+            if (string.IsNullOrWhiteSpace(sort)) return src; 
             var prop = typeof(T).GetProperty(sort, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
-            if (prop is null) return src; // campo inválido => no ordenar
+            if (prop is null) return src; 
 
             return string.Equals(order, "desc", StringComparison.OrdinalIgnoreCase)
                 ? src.OrderByDescending(x => prop.GetValue(x))
@@ -39,9 +38,9 @@ namespace FirstExam.Controllers
         public IActionResult GetAll(
            [FromQuery] int? page,
            [FromQuery] int? limit,
-           [FromQuery] string? sort,     // ejemplo: name | species | age
-           [FromQuery] string? order,    // asc | desc
-           [FromQuery] string? q       // búsqueda por nombre o email
+           [FromQuery] string? sort,     
+           [FromQuery] string? order,    
+           [FromQuery] string? q       
        )
         {
             var (p, l) = NormalizePage(page, limit);
