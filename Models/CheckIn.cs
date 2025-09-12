a3f45ac7ef48f2f05a81617797c095d1ee17333b
Majo <maria.sandoval.o@ucb.edu.bo>
2025-09-12 16:28:20 -0400

    Feat:CheckIn Model with Create and Update DTO

---
+++ Models/CheckIn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+public class CheckIn
+{
+
+    public Guid id { get; set; }
+
+    [Required, StringLength(8)]
+    public string BadgeCode { get; set; } = string.Empty;
+
+    [Required]
+    public DateTime Timestamp;
+}
+
+public record CreateCheckinDto
+{
+    [Required, StringLength(8)]
+    public string BadgeCode { get; set; } = string.Empty;
+
+    [Required]
+    public DateTime Timestamp;
+}
+public record UpdateCheckinDto
+{
+    [Required, StringLength(8)]
+    public string BadgeCode { get; set; } = string.Empty;
+
+    [Required]
+    public DateTime Timestamp;
+}
