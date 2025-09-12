9c17e4bfb79f663096394b304c405874463be9b4
unknown <luiguiandre2208@gmail.com>
2025-09-12 16:34:20 -0400

    feat:model Membership

---
+++ Models/Membership.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+public class Membership
+{
+    public Guid Id { get; set; }
+    public Guid MemberId { get; set; }
+
+    [Required, StringLength(100)]
+    public string Plan { get; set; } = string.Empty; // basic | pro | premium
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+    [Required, StringLength(10)] // active | expired | canceled
+    public string Status { get; set; } = string.Empty;
+}
+
+public record CreateMembershipDto
+{
+    [Required, StringLength(100)]
+    public string Plan { get; set; } = string.Empty; // basic | pro | premium
+    
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+
+    [Required, StringLength(10)] // active | expired | canceled
+    public string Status { get; set; } = string.Empty;
+}
+
+public record UpdateMembershipDto
+{
+    [Required, StringLength(100)]
+    public string Plan { get; set; } = string.Empty; // basic | pro | premium
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+
+    [Required, StringLength(10)] // active | expired | canceled
+    public string Status { get; set; } = string.Empty;
+
+}
