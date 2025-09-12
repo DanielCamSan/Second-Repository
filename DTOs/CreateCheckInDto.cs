51b77ad86360973d85d59b87b63b4ec8893c3f27
Tu Nombre <victor.medrano.z@ucb.edu.bo>
2025-09-12 17:06:29 -0400

    Fix: Initialize BadgeCode in CreateCheckInDto

--- DTOs/CreateCheckInDto.cs
+++ DTOs/CreateCheckInDto.cs
@@ -1,6 +1,10 @@
-﻿namespace FirstExam.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstExam.DTOs
 {
     public class CreateCheckInDto
     {
+        [Required(ErrorMessage = "BadgeCode is required")]
+        public string BadgeCode { get; set; }
     }
 }
