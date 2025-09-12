e90fb66af0f2146f64ebf938a2e41b367630b456
Adrian Claros <166774927+AdrianCV2345@users.noreply.github.com>
2025-09-12 16:35:56 -0400

    feat: Dtos of create and update created.

--- Models/Membership.cs
+++ Models/Membership.cs
@@ -19,3 +19,42 @@ public class Membership
     public string status { get; init; } = string.Empty;
 }
 
+
+///DTOs
+public record CreateMembershipDto
+{
+    public Guid id { get; init; }
+
+    public Guid MemberId { get; init; }
+
+    [Required, StringLength(100)]
+    public string plan { get; init; } = string.Empty;
+
+    [Required, StringLength(100)]
+    public DateTime StartTime { get; init; }
+
+    [Required, StringLength(100)]
+    public DateTime Endtime { get; init; }
+
+    [Required, StringLength(100)]
+    public string status { get; init; } = string.Empty;
+}
+
+public record UpdateMembershipDto
+{
+    public Guid id { get; init; }
+
+    public Guid MemberId { get; init; }
+
+    [Required, StringLength(100)]
+    public string plan { get; init; } = string.Empty;
+
+    [Required, StringLength(100)]
+    public DateTime StartTime { get; init; }
+
+    [Required, StringLength(100)]
+    public DateTime Endtime { get; init; }
+
+    [Required, StringLength(100)]
+    public string status { get; init; } = string.Empty;
+}
