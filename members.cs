20ac899d7d80be6995344d892162257e249f62da
leitocam <leonardo.camacho@ucb.edu.bo>
2025-09-12 16:33:51 -0400

    feat: member model creation

---
+++ members.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstExam
+{
+    public record Member(
+        Guid Id,
+        string Email,
+        string FullName,
+        bool Active
+    );
+
+    public class members
+    {
+    }
+}
