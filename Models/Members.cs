4360420df8469240f5030eed693253b66e258655
Fabio Garcia Meza <fabio.garciameza@ucb.edu.bo>
2025-09-12 16:51:58 -0400

    fix(Models): adding default true in active

--- Models/Members.cs
+++ Models/Members.cs
@@ -6,26 +6,35 @@ namespace FirstExam.Models
     public class Members
     {
         public Guid Id { get; set; }
-        [Required, StringLength(128)]
+
+        [Required, StringLength(128), EmailAddress]
         public string Email { get; set; } = string.Empty;
+
         [Required, StringLength(128)]
         public string Name { get; set; } = string.Empty;
-        public bool Active { get; set; }
+
+        public bool Active { get; set; } = true;
     }
+
     public record CreateMemberDto
     {
-        [Required, StringLength(128)]
+        [Required, StringLength(128), EmailAddress]
         public string Email { get; init; } = string.Empty;
+
         [Required, StringLength(128)]
-        public string Name { get; init; } = string.Empty ;
-        public bool IsActive { get; init; }
+        public string Name { get; init; } = string.Empty;
+
+        public bool IsActive { get; init; } = true;
     }
+
     public record UpdateMemberDto
     {
-        [Required, StringLength(128)]
+        [Required, StringLength(128), EmailAddress]
         public string Email { get; init; } = string.Empty;
+
         [Required, StringLength(128)]
-        public string Name { get; init; } = String.Empty;
+        public string Name { get; init; } = string.Empty;
+
         public bool IsActive { get; init; }
     }
 }
