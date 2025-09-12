0c64b934c0338f8dd81af009fa2122a17ee14bba
Flora <Flora.ortiz@ucb.edu.bo>
2025-09-12 16:30:40 -0400

    feat: creating Member Model

---
+++ Models/Member.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+public class Member
+{
+    public Guid Id { get; set; }
+
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required, StringLength(200)]
+    public string FullName { get; set; } = string.Empty;
+
+    [Required]
+    bool Active { get; set; }
+}
