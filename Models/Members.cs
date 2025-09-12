7d0b14fd186b1a70d9df2c680af29dae44e46ea5
Caleb <calebbaving12@gmail.com>
2025-09-12 16:17:36 -0400

    feat: the member DTOS have been created

---
+++ Models/Members.cs
@@ -0,0 +1,39 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Member
+{
+    public Guid Id { get; set; }
+
+    [Required, StringLength(100)]
+    public string FullName { get; set; } = string.Empty;
+
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+
+    public bool Active { get; set; }
+}
+public record CreateMemberDto
+{
+    [Required, StringLength(100)]
+    public string FullName { get; init; } = string.Empty;
+
+    [Required, EmailAddress]
+    public string Email { get; init; } = string.Empty;
+
+    [Required]
+    public bool Active { get; init; }
+}
+public record UpdateMemberDto
+{
+    [Required, StringLength(100)]
+    public string FullName { get; init; } = string.Empty;
+
+    [Required, EmailAddress]
+    public string Email { get; init; } = string.Empty;
+
+    [Required]
+    public bool Active { get; init; }
+}
