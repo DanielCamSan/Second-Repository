fc5ae8a1ece26c7c820af7f93e4290198d15e340
Isabel Hidalgo <hidalgoavilaisabel@gmail.com>
2025-09-26 18:06:39 -0400

    fix: program

--- Program.cs
+++ Program.cs
@@ -1,19 +1,26 @@
-using Microsoft.AspNetCore.RateLimiting;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("MiPoliticaCros", policy =>
+    options.AddPolicy("MiPoliticaCors", policy =>
     {
-        policy.WithOrigins("https://localhost:7162", "http://127.0.0.1:5500").AllowAnyMethod().AllowAnyHeader();
+        policy.WithOrigins("https://localhost:7162", "http://127.0.0.1:5500")
+              .AllowAnyMethod()
+              .AllowAnyHeader();
     });
 });
 
 builder.Services.AddControllers();
+
 var app = builder.Build();
+
 app.UseHttpsRedirection();
+
 app.UseCors("MiPoliticaCors");
+
 app.UseAuthorization();
+
+app.MapControllers();
+
 app.Run();
