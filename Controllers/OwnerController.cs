af389e6cc627c207bcf8db1521ceffccc83efb1b
unknown <nicole.herbas@ucb.edu.bo>
2025-09-26 16:27:19 -0400

    feat: new owner controller

---
+++ Controllers/OwnerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstExam.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OwnerController : ControllerBase
+    {
+
+
+    }
+}
