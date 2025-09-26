4f28926c4a133d4b78db74ce54f12100fde66946
jmss <juanma.soruco@gmail.com>
2025-09-26 16:28:08 -0400

    feat:model

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace FirstExam.Controllers
+{
+    public class AppointmentController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Appointment.cs
@@ -0,0 +1,34 @@
+﻿using FirstExam.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace FirstExam.Models
+{
+    public class Appointment
+    {
+        [Required] public Guid Id { get; set; }
+        [Required] public Guid PetId { get; set; }
+        [Required] public DateOnly ScheduledAt { get; set; } // fecha y hora agendada  
+        [Required] public String Reason { get; set; }// vacunación, control, cirugía, etc. 
+        [Required] public String Status { get; set; } = "scheduled";// scheduled | completed | canceled | no_show 
+        public String? Notes { get; set; }
+    }
+    public record CreateAppointmentDto
+    {
+        [Required] public DateOnly ScheduledAt { get; init; } // fecha y hora agendada  
+        [Required] public String Reason { get; init; }// vacunación, control, cirugía, etc. 
+        [Required] public String Status { get; init; } = "scheduled";// scheduled | completed | canceled | no_show 
+        public String? Notes { get; init; }
+    }
+
+    public record UpdateAppointmentDto
+    {
+        [Required] public DateOnly ScheduledAt { get; init; } // fecha y hora agendada  
+        [Required] public String Reason { get; init; }// vacunación, control, cirugía, etc. 
+        [Required] public String Status { get; init; } = "scheduled";// scheduled | completed | canceled | no_show 
+        public String? Notes { get; init; }
+    }
+
+}
