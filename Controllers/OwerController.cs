f6b5f07314701faf35841b10682b94abc512c6fd
Brenda Medrano <brenda.medrano@ucb.edu.bo>
2025-09-26 18:05:53 -0400

    create controller

---
+++ Controllers/OwerController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstExam.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/[controller]")]
+    public class OwerController : Controller
+    {
+        private static readonly List<Owners> _owners = new();
+
+        [HttpGet]
+        public IActionResult GetAll(
+            [FromQuery] int page = 1,
+            [FromQuery] int limit = 10,
+            [FromQuery] string? sort = "Email",
+            [FromQuery] string order = "asc")
+        { 
+            if(page< 1){
+                page = 1;
+            }
+            if (limit < 10) {
+                limit = 10;
+            }
+            if(limit > 100)
+            {
+                limit = 100;
+            }
+            if(order != "asc" && order != "desc")
+            {
+                order = "asc";
+            }
+            var query = _owners.AsQueryable();
+            query = sort switch
+            {
+                "Email" => order == "asc" ? query.OrderBy(a => a.Email) : query.OrderByDescending(a => a.Email)
+            };
+            var total = query.Count();
+            var items = query.Skip((page-1)*limit).Take(total).ToList();
+            return Ok(new { data = items, meta = new { page, limit, total } });
+
+        }
+        [HttpGet("{id:guid}")]
+        public ActionResult GetById(Guid id)
+        {
+            var item = _owners.FirstOrDefault(o => o.Id == id);
+            return item == null ? NotFound() : View(item);
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateOwerDto dto)
+        {
+            if (!ModelState.IsValid) return ValidationProblem(ModelState);
+            var item = new Owners { Id = dto.Id, Active = true };
+            _owners.Add(item);
+            return CreatedAtAction(nameof(GetById), new { id = item }, item);
+        }
+        [HttpDelete]
+        public IActionResult DeleteById(Guid id)
+        {
+            var item = _owners.FirstOrDefault(a => a.Id == id);
+
+            if (item == null) return NotFound();
+            _owners.Remove(item);
+            return NoContent();
+        }
+    }
+}
--- Owners.cs
+++ Owners.cs
@@ -1,6 +1,28 @@
-﻿namespace FirstExam
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+namespace FirstExam
 {
-    public class owners
+    public class Owners
     {
+        public Guid Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public bool Active { get; set; } = true;
+    }
+    public class CreateOwerDto
+    {
+        public Guid Id { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }= string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public bool Active { get; set; } = true;
+
+    }
+    public class UpdateOwerDto
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public bool Active { get; set; } = true;
     }
 }
