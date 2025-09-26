599231f17ed0e25277599da08c814a5dbaf81485
unknown <dabnerorozco123@gmail.com>
2025-09-26 16:51:16 -0400

    Adding allowed Values into Pet Model

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -7,12 +7,12 @@ public class Pet
         
     [Required, StringLength(100)]
     public string Name { get; set; } = string.Empty;
-    [Required, StringLength(50)]
+    [Required, StringLength(50), AllowedValues("dog", "cat", "bird", "reptile", "other")]
     public string Species { get; set; } = string.Empty;
     [Required, StringLength(50)]
     public string Breed { get; set; } = string.Empty;
     public DateTime BirthDate { get; set; }
-    [Required, StringLength(50)]
+    [Required, StringLength(50), AllowedValues("male", "female")]
     public string Sex { get; set; } = string.Empty;
     [Range(0,100)]
     public decimal? WeightKg { get; set; }
