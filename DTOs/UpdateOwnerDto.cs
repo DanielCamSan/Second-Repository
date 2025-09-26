cf6d577a86ef7cafa8ec813a151c579358304b53
fabio-gmoo <fabio.garciameza@ucb.edu.bo>
2025-09-26 16:56:53 -0400

    feat(DTOs) :adding updateOwnerDTO.cs

---
+++ DTOs/UpdateOwnerDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstExam.Models.DTOs
+{
+    public class UpdateOwnerDto
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required, StringLength(100)]
+        public string FullName { get; set; } = string.Empty;
+        public bool Active { get; set; }
+    }
+}
