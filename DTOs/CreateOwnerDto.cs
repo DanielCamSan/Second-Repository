4906bee7da2d0e40458b24bc186c0dd22754f239
fabio-gmoo <fabio.garciameza@ucb.edu.bo>
2025-09-26 16:34:08 -0400

    feat(DTOs) :Adding CreateOwnerDTO.cs

---
+++ DTOs/CreateOwnerDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VetClinic.Models.DTOs
+{
+    public class CreateOwnerDto
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required, StringLength(100)]
+        public string FullName { get; set; } = string.Empty;
+        public bool Active { get; set; } = true;
+    }
+}
