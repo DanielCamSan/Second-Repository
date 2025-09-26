a5f6f58b7b615b2aafff0ba6c52a5443c53d41a2
unknown <adrianmercado782@gmail.com>
2025-09-26 16:59:56 -0400

    feat AppointmentsController with integrated get and listed

--- Controllers/AppointmentsController.cs
+++ Controllers/AppointmentsController.cs
@@ -1,6 +1,23 @@
-﻿namespace FirstExam.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FirstExam.Controllers
 {
-    public class AppointmentsController
+    [ApiController]
+    [Route("api/v1/appointments")]
+    public class AppointmentsController : ControllerBase
     {
+        private static readonly List<Appointment> _appointments = new();
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var appointment = _appointments.FirstOrDefault(a => a.Id == id);
+            if (appointment == null)
+                return NotFound(new { error = "Appointment not found", status = 404 });
+
+            return Ok(appointment);
+        }
     }
 }
