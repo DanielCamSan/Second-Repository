6d04d8f30606074bc0d0fdc419c9df23505d830f
Flora Ortiz <flora.ortiz@ucb.edu.bo>
2025-09-26 16:36:49 -0400

    feat: creating mock list

--- Controllers/AppointmentsController.cs
+++ Controllers/AppointmentsController.cs
@@ -7,6 +7,12 @@ namespace FirstExam.Controllers
     [Route ("api/[controller]")]
     public class AppointmentsController : ControllerBase
     {
+        private static readonly List<Appointment> _appointments = new()
+        {
+            new Appointment {Id = Guid.NewGuid(), PetId = Guid.NewGuid(), ScheduledAt = DateTime.Now, Status = "scheduled", Reason = "control" },
+            new Appointment {Id = Guid.NewGuid(), PetId = Guid.NewGuid(), ScheduledAt = DateTime.Now, Status = "completed", Reason = "control" },
+            new Appointment {Id = Guid.NewGuid(), PetId = Guid.NewGuid(), ScheduledAt = DateTime.Now, Status = "canceled", Reason = "vacunacion", Notes = "Alergia" }
+        };
 
     }
 }
