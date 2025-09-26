cbab2f00ff75fd04ed9f584fb85dc6404a873792
Maria Lozada <marialozada@ucb.edu.bo>
2025-09-26 17:08:33 -0400

    feat: adding order to OwnersController

--- Controllers/OwnersController.cs
+++ Controllers/OwnersController.cs
@@ -1,6 +1,7 @@
 ﻿using FirstExam.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 using System.Reflection.Metadata.Ecma335;
 //using System.Reflection;
 
@@ -24,8 +25,19 @@ namespace FirstExam.Controllers
         {
 
             var p= page.GetValueOrDefault(1); if (p < 1) p = 1;
-            var l = limit.GetValueOrDefault(10); if (l < 1) l = 1; if (l > 100) l = 100;
+            var l = limit.GetValueOrDefault(10); if (l < 1) l = 10; if (l > 100) l = 100;
             return (p, l);
+        }
+
+        private static IEnumerable<T> OrderByProp<T> (IEnumerable<T> src, string? sort, string? order)
+        {
+            if(string.IsNullOrWhiteSpace(sort)) return src;
+            var prop = typeof (T).GetProperty(sort, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+            if(prop == null) return src;
+            return string.Equals(order, "desc", StringComparison.OrdinalIgnoreCase)
+                ? src.OrderByDescending(x => prop.GetValue(x))
+                : src.OrderBy(x => prop.GetValue(x));
+
         }
 
 
