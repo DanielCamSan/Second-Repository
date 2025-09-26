dca50ef5cdf5ecccf940b858712a4c9d6c489389
Flora Ortiz <flora.ortiz@ucb.edu.bo>
2025-09-26 16:40:55 -0400

    feat: pagination normalize parameters

--- Controllers/AppointmentsController.cs
+++ Controllers/AppointmentsController.cs
@@ -14,5 +14,13 @@ namespace FirstExam.Controllers
             new Appointment {Id = Guid.NewGuid(), PetId = Guid.NewGuid(), ScheduledAt = DateTime.Now, Status = "canceled", Reason = "vacunacion", Notes = "Alergia" }
         };
 
+        private static (int page, int limit) NormalizePage(int? page, int? limit)
+        {
+            var p = page.GetValueOrDefault(1); if (p < 1) p = 1;
+            var l = page.GetValueOrDefault(10); if(l < 1) l = 1; if (l>100) l = 100;
+            return (p, l);
+        }
+
+
     }
 }
