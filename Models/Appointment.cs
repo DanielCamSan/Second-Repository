5c1c13842ef182313fb5741bb781c56ba357a468
Flora Ortiz <flora.ortiz@ucb.edu.bo>
2025-09-26 16:20:10 -0400

    feat: creating model

---
+++ Models/Appointment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+public class Appointment
+{
+    public Guid Id { get; set; }
+    public Guid PetId { get; set; }
+    public DateTime ScheduledAt { get; set; }
+    public string Reason {  get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+    public string Notes { get; set; } = string.Empty;
+}
