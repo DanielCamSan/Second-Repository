1920c9b100407926fd0c93610e447d364803532c
Maria Lozada <marialozada@ucb.edu.bo>
2025-09-26 16:39:49 -0400

    feat: adding owners DTOs

--- Models/Owner.cs
+++ Models/Owner.cs
@@ -1,6 +1,43 @@
-﻿namespace FirstExam.Models
+﻿namespace FirstExam.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+
+public class Owner
 {
-    public class Owner
-    {
-    }
+        public Guid Id { get; set; }
+
+
+        [Required, StringLength(100)]
+        public string Email { get; set; }
+
+        [Required, StringLength(100)]
+        public string FullName { get; set; }
+
+        public bool Active { get; set; } = true;
+
 }
+
+public record CreateOwnerDto
+{
+    [Required, StringLength(100)]
+    public string Email { get; init; } = string.Empty;
+
+    [Required, StringLength(100)]
+    public string FullName { get; init; } = string.Empty;
+    
+    public bool Active { get; set; } = true;
+}
+
+public record UpdateOwnerDto
+{
+    [Required, StringLength(100)]
+    public string Email { get; init; } = string.Empty;
+
+    [Required, StringLength(100)]
+    public string FullName { get; init; } = string.Empty;
+
+    public bool Active { get; set; } = true;
+
+}
+
