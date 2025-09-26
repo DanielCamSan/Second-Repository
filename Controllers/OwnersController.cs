dbf71fe2fd06322bed721b343035acd8bf2d429a
CalebBav234 <calebmbaving@gmail.com>
2025-09-26 17:51:40 -0400

    fix: OwmersController limit has been fixed

--- Controllers/OwnersController.cs
+++ Controllers/OwnersController.cs
@@ -16,7 +16,7 @@ namespace Owners.Controllers
         private static (int page, int limit) NormalizePage(int? page, int? limit)
         {
             var p = page.GetValueOrDefault(1); if (p < 1) p = 1;
-            var l = limit.GetValueOrDefault(10); if (l < 1) l = 1; if (l > 100) l = 100;
+            var l = limit.GetValueOrDefault(10); if (l < 1) l = 10; if (l > 100) l = 100;
             return (p, l);
         }
         private static IEnumerable<T> OrderByProp<T>(IEnumerable<T> src, string? sort, string? order)
