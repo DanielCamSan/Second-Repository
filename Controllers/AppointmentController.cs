8a54781075127add99a096b61339f3bb29236e8a
jmss <juanma.soruco@gmail.com>
2025-09-26 16:46:24 -0400

    feat: delete appointments

--- Controllers/AppointmentController.cs
+++ Controllers/AppointmentController.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FirstExam.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System.Reflection;
 
 namespace FirstExam.Controllers
 {
+    [ApiController]
+    [Route("api/[Controller]")]
     public class AppointmentController : ControllerBase
     {
-        public IActionResult Index()
+        private static readonly List<Appointment> _appointments = new(){
+                new Appointment{Id=Guid.NewGuid(),PetId=Guid.NewGuid(),ScheduledAt=DateTime.Now, Reason="vaccinate",Status="scheduled" },
+            new Appointment{Id=Guid.NewGuid(),PetId=Guid.NewGuid(),ScheduledAt=DateTime.Now, Reason="vaccinate",Status="scheduled" }
+        };
+
+        [HttpDelete("{Id:Guid}")]
+        public ActionResult Delete(Guid id)
         {
-            return View();
+            var count = _appointments.RemoveAll(x => x.Id == id);
+            if (count == 0) { return NotFound(new { error = "Appointment Not Found", status = 404 }); }
+            else { return NoContent(); }
         }
     }
 }
