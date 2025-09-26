eb3616729b5dff4c9c12460b45b25522e5cde36e
unknown <nicole.herbas@ucb.edu.bo>
2025-09-26 16:31:50 -0400

    feat: list with owners

--- Controllers/OwnerController.cs
+++ Controllers/OwnerController.cs
@@ -6,7 +6,11 @@ namespace FirstExam.Controllers
     [Route("[controller]")]
     public class OwnerController : ControllerBase
     {
-
-
+        private static readonly List<Owner> Owners = new()
+        {
+            new Owner { Id = Guid.NewGuid(),Email = "JuanPerez@gmail.com", FullName = "Juan Perez", Active = true },
+            new Owner { Id = Guid.NewGuid(),Email = "MariaGomez@gmail.com", FullName = "Maria Gomez", Active = true },
+            new Owner { Id = Guid.NewGuid(),Email = "CarlosSanchez@gmail.com", FullName = "Carlos Sanchez", Active = false }
+        };
     }
 }
