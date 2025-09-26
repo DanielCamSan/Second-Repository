edad90edafff23729d91534375680989cc341e94
renesoria <rene.soria@ucb.edu.bo>
2025-09-26 17:17:01 -0400

    feat: adding list of pets

---
+++ Controllers/PetsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+namespace FirstExam.Controllers
+
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class PetsController : ControllerBase
+    {
+        private static readonly List<Pet> _pets = new()
+{
+    new Pet { Id = Guid.NewGuid(), OwnerId = Guid.NewGuid(), Name = "Preto", Species = "dog", Breed = "Sharpei", Birthdate = new DateTime(2020, 5, 12), Sex = "male", WeigthKg = 22.5m },
+    new Pet { Id = Guid.NewGuid(), OwnerId = Guid.NewGuid(), Name = "Mimi", Species = "cat", Breed = "Siamese", Birthdate = new DateTime(2019, 8, 20), Sex = "female", WeigthKg = 4.28m },
+    new Pet { Id = Guid.NewGuid(), OwnerId = Guid.NewGuid(), Name = "Kiko", Species = "bird", Breed = "Parrot", Birthdate = new DateTime(2021, 1, 15), Sex = "male", WeigthKg = 0.3m },
+    new Pet { Id = Guid.NewGuid(), OwnerId = Guid.NewGuid(), Name = "Lola", Species = "dog", Breed = "Cooker", Birthdate = new DateTime(2018, 11, 5), Sex = "female", WeigthKg = 28.0m },
+    new Pet { Id = Guid.NewGuid(), OwnerId = Guid.NewGuid(), Name = "Spike", Species = "reptile", Breed = "Iguana", Birthdate = new DateTime(2022, 3, 30), Sex = "male", WeigthKg = 5.1m }
+};
+
+    }
+}
