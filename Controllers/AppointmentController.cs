9ab195b90e875aa02f45e750d45d859d1d006fb1
jmss <juanma.soruco@gmail.com>
2025-09-26 17:10:44 -0400

    feat: getById appointments

--- Controllers/AppointmentController.cs
+++ Controllers/AppointmentController.cs
@@ -14,8 +14,24 @@ namespace FirstExam.Controllers
             new Appointment{Id=Guid.NewGuid(),PetId=Guid.NewGuid(),ScheduledAt=DateTime.Now, Reason="vaccinate",Status="scheduled" }
         };
 
+        [HttpGet]
+        public ActionResult<Appointment> GetAll()
+        {
+            var result = _appointments.FirstOrDefault(x => x.Id == id);
+            if (result == null) { return NotFound(new { error = "Appointment Not Found", status = 404 }); }
+            return Ok(result);
+        }
+
+        [HttpGet("{id:Guid}")]
+        public ActionResult<Appointment> GetById(Guid id)
+        {
+            var result=_appointments.FirstOrDefault(x => x.Id == id);
+            if (result == null) { return NotFound(new { error = "Appointment Not Found", status = 404 }); }
+            return Ok(result);
+        }
+
         [HttpPost]
-        public ActionResult<Appointment> Update(CreateAppointmentDto dto)
+        public ActionResult<Appointment> Update([FromBody] CreateAppointmentDto dto)
         {
             if (!ModelState.IsValid) { return ValidationProblem(ModelState); }
             var appointment = new Appointment
@@ -32,16 +48,16 @@ namespace FirstExam.Controllers
             return CreatedAtAction(nameof(GetOne), new { id = appointment.Id }, appointment};
         }
 
-        [HttpPut("{Id:Guid}/{Id2:Guid}")]
-        public ActionResult<Appointment> Update(Guid id, Guid id2, UpdateAppointmentDto dto)
+        [HttpPut("{Id:Guid}")]
+        public ActionResult<Appointment> Update(Guid id, [FromBody] UpdateAppointmentDto dto)
         {
             if (!ModelState.IsValid) { return ValidationProblem(ModelState); }
-            var index = _appointments.FindIndex(x => x.Id == id && x.PetId==id2);
+            var index = _appointments.FindIndex(x => x.Id == id);
             if (index == -1) {return NotFound(new { error = "Appointment Not Found", status = 404 }); }
             var update = new Appointment
             {
                 Id = id,
-                PetId = id2,
+                PetId = _appointments[index].PetId,
                 Reason = dto.Reason,
                 Status = dto.Status
             };
@@ -59,3 +75,4 @@ namespace FirstExam.Controllers
         }
     }
 }
+
