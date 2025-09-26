d4bf48573dcae2f76bbaaf60144976aa6c0e9808
CalebBav234 <calebmbaving@gmail.com>
2025-09-26 17:19:43 -0400

    feat: the HttpDelete  has been created for owners

--- Controllers/OwnersController.cs
+++ Controllers/OwnersController.cs
@@ -62,7 +62,13 @@ namespace Owners.Controllers
             _owners[index] = updated;
             return Ok(updated);
         }
-        
+        [HttpDelete("{id:guid}")]
+        public IActionResult Delete(Guid id)
+        {
+            var removed = _owners.RemoveAll(a => a.Id == id);
+            return removed == 0 ? NotFound(new { error = "Owner not found", status = 404 }) : NoContent();
+        }
+
 
 
 
