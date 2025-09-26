30bfb8724ee6e9ccb406f591383ac441bb2cc180
AdrianCV2345 <Adrianclarosv3@gmail.com>
2025-09-26 16:33:30 -0400

    feat:List mock created.

---
+++ Controllers/AppointmentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System.Runtime.Intrinsics.X86;
+
+
+
+namespace FirstExam.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AppointmentsController : ControllerBase
+    {
+        public static readonly List<Appointments> _appointments = new()
+        {
+         //  new Appointments {Id=Guid.NewGuid(),PetId=Guid.NewGuid(),Schedule=DateTime.Now,Reason=},
+        };
+
+    }
+}
