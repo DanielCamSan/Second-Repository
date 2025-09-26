cd06dbe136ee8a8643fff27b5090d71990bfe475
jmss <juanma.soruco@gmail.com>
2025-09-26 16:53:43 -0400

    feat: update appointments

--- Controllers/AppointmentController.cs
+++ Controllers/AppointmentController.cs
@@ -14,6 +14,23 @@ namespace FirstExam.Controllers
             new Appointment{Id=Guid.NewGuid(),PetId=Guid.NewGuid(),ScheduledAt=DateTime.Now, Reason="vaccinate",Status="scheduled" }
         };
 
+        [HttpPut("{Id:Guid}/{Id2:Guid}")]
+        public ActionResult Update(Guid id, Guid id2, UpdateAppointmentDto dto)
+        {
+            var index = _appointments.FindIndex(x => x.Id == id && x.PetId==id2);
+            if (index == -1) {return NotFound(new { error = "Appointment Not Found", status = 404 }); }
+            var update = new Appointment
+            {
+                Id = id,
+                PetId = id2,
+                Reason = dto.Reason,
+                Status = dto.Status
+
+            };
+            _appointments[index] = update;
+            return Ok(update);
+        }
+
         [HttpDelete("{Id:Guid}")]
         public ActionResult Delete(Guid id)
         {
