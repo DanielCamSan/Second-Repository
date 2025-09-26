c101bddca0c29cb85f1ee28a9fe4f6c6a714e9c2
soliz soriano Maria Rene <maria.soliz.s@ucb.edu.bo>
2025-09-26 16:33:09 -0400

    feat: Owner model and DTOs

---
+++ Models/Owner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstExam.Models
+{
+    public class owner
+    {
+        public Guid Id { get; set; }
+        [Required, StringLength(200)]
+        public string Email { get; set; } = string.Empty;
+        [Required, StringLength(200)]
+        public string FullName { get; set; } = string.Empty;
+        [Required, StringLength(200)]
+        public bool Active { get; set; }=true;
+
+        public record CreateOwnerDto
+        {
+            [Required, StringLength(200)]
+            public string Email { get; init; }
+            [Required, StringLength(200)]
+            public string FullName { get; init; }
+            [Required, StringLength(200)]
+            public bool Active { get; init; }
+        }
+        public record UpdateOwnerDto
+        {
+            [Required, StringLength(200)]
+            public string Email { get; set; }
+            [Required, StringLength(200)]
+            public string FullName { get; set; }
+            [Required, StringLength(200)]
+            public bool Active { get; set; }
+        }
+
+
+
+    }
+}
+
