a417420faf0138035057dbc380b51e9672c73e72
unknown <dabnerorozco123@gmail.com>
2025-09-26 17:18:04 -0400

    feat: Creating PetsController

---
+++ Controllers/PetsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstExam.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class PetsController : ControllerBase
+    {
+        private static readonly List<Pet> _pets = new()
+        {
+            new Pet {Id = Guid.NewGuid(), Name = "Rocky", Species = "dog", Sex = "male" },
+            new Pet {Id = Guid.NewGuid(), Name = "Cookie", Species = "cat", Sex = "female" },
+            new Pet {Id = Guid.NewGuid(), Name = "Red", Species = "bird", Sex = "male" },
+        };
+
+
+    }
+}
+
