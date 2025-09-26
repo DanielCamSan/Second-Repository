dff0b63bdf7fef936fd872b64f172fbfd6fdbad9
majo <maria.sandoval.o@ucb.edu.bo>
2025-09-26 16:25:13 -0400

    Feat: Class Pet creation

---
+++ Models/Pet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Pet
+{
+    public Guid Guid { get; set; }
+    public Guid OwnerId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Species { get; set; } = string.Empty;
+    public string Breed { get; set; } = string.Empty;
+    public DateTime Birthdate { get; set; }
+    public string Sex { get; set; } = string.Empty;
+    public decimal? WeightKg { get; set; }
+}
