5777cb7ec6c393e6ea4d1622aa2b16e72365914a
Camilo <camilorodriguezandre@gmail.com>
2025-09-26 16:56:57 -0400

    Implementation of pagination and ordanation

--- Models/Apointment.cs
+++ Models/Apointment.cs
@@ -1,6 +1,31 @@
-﻿namespace FirstExam.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Apointment
 {
-    public class Apointment
-    {
-    }
+    public Guid Id { get; set; }
+    [Required]
+    public DateTime ScheduledAt { get; set; }
+    public string Reason { get; set; }
+    public string Status { get; set; }
+    public string? Notes { get; set; }
+}
+
+
+public record CreateApointmentDto
+{
+    [Required]
+    public DateTime ScheduledAt { get; set; }
+    public string Reason { get; set; }
+    public string Status { get; set; }
+    public string? Notes { get; set; }
+}
+
+public record UpdateApointmentDto
+{
+    [Required]
+    public DateTime ScheduledAt { get; set; }
+    public string Reason { get; set; }
+    public string Status { get; set; }
+    public string? Notes { get; set; }
 }
