25316bb932ee7f5dd3b404f70eca5e625ac177e8
AdrianCV2345 <Adrianclarosv3@gmail.com>
2025-09-26 17:46:48 -0400

    feat: Cors created.

--- Program.cs
+++ Program.cs
@@ -2,37 +2,23 @@ using Microsoft.AspNetCore.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-/*
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("MiPoliticaCors", policy =>
     {
-        policy.WithOrigins("",
-                  "http:127.0.0.1:5500")
+        policy.WithOrigins("https://localhost:7047",
+                  "http://127.0.0.1:5500")
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
 });
-builder.Services.AddRateLimiter(options =>
-{
-    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
-    options.OnRejected = async (ctx, ct) =>
-    await ctx.HttpContext.Response.WriteAsync("Too many attempts, try it in one minute", ct);
-    options.AddFixedWindowLimiter("default", config =>
-    {
-        config.PermitLimit = 3;                    
-        config.Window = TimeSpan.FromMinutes(1);    
-        config.QueueLimit = 0;                      
-    });
-});
-*/
+
+
 
 builder.Services.AddControllers();
 var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseCors("MiPoliticaCors");
 app.UseAuthorization();
-app.UseRateLimiter();
 app.MapControllers().RequireRateLimiting("default");
 app.Run();
