a8508a8159cf26641939aa1140cba865bcbd5fba
Flora Ortiz <flora.ortiz@ucb.edu.bo>
2025-09-26 16:32:38 -0400

    feat: creating AppointmentsController

---
+++ Controllers/AppointmentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace FirstExam.Controllers
+{
+    [ApiController]
+    [Route ("api/[controller]")]
+    public class AppointmentsController : ControllerBase
+    {
+
+    }
+}
