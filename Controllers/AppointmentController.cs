15c71a51acaebbad1df1a441c2ea37e72edf683e
jmss <juanma.soruco@gmail.com>
2025-09-26 17:01:44 -0400

    feat: create appointments

--- Controllers/AppointmentController.cs
+++ Controllers/AppointmentController.cs
@@ -14,9 +14,28 @@ namespace FirstExam.Controllers
             new Appointment{Id=Guid.NewGuid(),PetId=Guid.NewGuid(),ScheduledAt=DateTime.Now, Reason="vaccinate",Status="scheduled" }
         };
 
-        [HttpPut("{Id:Guid}/{Id2:Guid}")]
-        public ActionResult Update(Guid id, Guid id2, UpdateAppointmentDto dto)
+        [HttpPost]
+        public ActionResult<Appointment> Update(CreateAppointmentDto dto)
         {
+            if (!ModelState.IsValid) { return ValidationProblem(ModelState); }
+            var appointment = new Appointment
+            {
+                Id = Guid.NewGuid(),
+                PetId = Guid.NewGuid(),
+                ScheduledAt = DateTime.Now,
+                Reason = dto.Reason,
+                Status = dto.Status,
+                Notes = ""
+            };
+            if (dto.Notes != null) { appointment.Notes = dto.Notes; }
+            _appointments.Add(appointment);
+            return CreatedAtAction(nameof(GetOne), new { id = appointment.Id }, appointment};
+        }
+
+        [HttpPut("{Id:Guid}/{Id2:Guid}")]
+        public ActionResult<Appointment> Update(Guid id, Guid id2, UpdateAppointmentDto dto)
+        {
+            if (!ModelState.IsValid) { return ValidationProblem(ModelState); }
             var index = _appointments.FindIndex(x => x.Id == id && x.PetId==id2);
             if (index == -1) {return NotFound(new { error = "Appointment Not Found", status = 404 }); }
             var update = new Appointment
@@ -25,14 +44,14 @@ namespace FirstExam.Controllers
                 PetId = id2,
                 Reason = dto.Reason,
                 Status = dto.Status
-
             };
+            if (dto.Notes != null) { update.Notes = dto.Notes; }
             _appointments[index] = update;
             return Ok(update);
         }
 
         [HttpDelete("{Id:Guid}")]
-        public ActionResult Delete(Guid id)
+        public IActionResult Delete(Guid id)
         {
             var count = _appointments.RemoveAll(x => x.Id == id);
             if (count == 0) { return NotFound(new { error = "Appointment Not Found", status = 404 }); }
