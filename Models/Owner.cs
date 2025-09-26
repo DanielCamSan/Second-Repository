709ac36bd7e40be5142595c105124b337162899f
Luiscuac <luguiandre2208@gmail.com>
2025-09-26 16:29:15 -0400

    feat:Owner Model with Dtos

---
+++ Models/Owner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Owner
+{
+    public Guid Id { get; set; }
+
+    [Required, StringLength(100)]
+    public string Email { get; set; }
+    [Required, StringLength(50)]
+    public string FullName { get; set; }
+  
+    public bool Active {  get; set; }
+}
+public record CreateOwnerDto
+{
+ 
+
+    [Required, StringLength(100)]
+    public string Email { get; set; } = string.Empty;
+    [Required, StringLength(50)]
+    public string FullName { get; set; } = string.Empty;
+
+    public bool Active { get; set; }
+}
+
+public record UpdateOwnerDto
+{
+
+
+    [Required, StringLength(100)]
+    public string Email { get; set; } = string.Empty;
+    [Required, StringLength(50)]
+    public string FullName { get; set; } = string.Empty;
+
+    public bool Active { get; set; }
+}
