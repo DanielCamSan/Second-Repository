2bd71f5828ed1484751ef9e86d8fbe0ccbb808a3
Ricardo <ricardo.andrade@ucb.edu.bo>
2025-09-26 16:40:21 -0400

    feat: create dtos

--- Models/Owner.cs
+++ Models/Owner.cs
@@ -4,8 +4,20 @@ using System.ComponentModel.DataAnnotations;
     public class Owner
     {
     public Guid Id { get; set; }
-    public string FullName { get; set; }
-    public string Email { get; set; }
-    public bool Active { get; set; }
-    }
+    public string FullName { get; set; }=string.Empty;
+    public string Email { get; set; } = string.Empty;
+
+    public bool Active { get; set; } 
+}
 //DTO 
+public record CreateOwnerDto
+{
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+}
+public record UpdateOwnerDto
+{
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+}
