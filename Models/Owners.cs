c70d71471bf41e1796dfb29dd4190d558534b7c0
fabio-gmoo <fabio.garciameza@ucb.edu.bo>
2025-09-26 16:31:39 -0400

    feat(Models) :Adding Owners.cs

---
+++ Models/Owners.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace VetClinicApi.Models
+{
+    public class Owner
+    {
+        public Guid Id { get; set; }
+        [Required, StringLength(200)]
+        public string Email { get; set; } = string.Empty;
+        [Required, StringLength(200)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public bool Active { get; set; } = true;
+    }
+}
