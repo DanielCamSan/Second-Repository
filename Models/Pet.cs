c4ac760b3b4736b7faffc4fd26a4778a81401b81
unknown <dabnerorozco123@gmail.com>
2025-09-26 16:36:45 -0400

    feat: Creating Models folder with Pet Model

---
+++ Models/Pet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+public class Pet
+{
+    public Guid Id { get; set; }
+    public Guid OwnerId { get; set; }
+        
+    [Required, StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+    [Required, StringLength(50)]
+    public string Species { get; set; } = string.Empty;
+    [Required, StringLength(50)]
+    public string Breed { get; set; } = string.Empty;
+    public DateTime BirthDate { get; set; }
+    [Required, StringLength(50)]
+    public string Sex { get; set; } = string.Empty;
+    [Range(0,100)]
+    public decimal? WeightKg { get; set; }
+}
+
