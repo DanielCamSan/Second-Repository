d62a9e2f6f4ea1967d66cdc00fceccff7991cce1
Ricardo <ricardo.andrade@ucb.edu.bo>
2025-09-26 16:27:38 -0400

    feat: create model owner.cs

---
+++ Models/Owner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+    public class Owner
+    {
+    public Guid Id { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public bool Active { get; set; }
+    }
+//DTO 
