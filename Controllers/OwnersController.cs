9c6fb615f8b6d4a5e3413909cd53aacd659ca476
Ricardo <ricardo.andrade@ucb.edu.bo>
2025-09-26 16:52:14 -0400

    feat: create owners Controller

---
+++ Controllers/OwnersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+namespace FirstExam.Controllers
+{
+    [ApiController]
+    [Route("api/[Controlller]")]
+    public class OwnersController : ControllerBase 
+    {
+        private static readonly List<Owner> _owners = new()
+        {
+new Owner {Id=Guid.NewGuid(),Email="flora@gmail.com",FullName="Flora Ortiz",Active=true},
+new Owner {Id=Guid.NewGuid(),Email="Dabner@gmail.com",FullName="Dabner Orozco",Active=true},
+new Owner {Id=Guid.NewGuid(),Email="Andy@gmail.com",FullName="Ricard Andrade",Active=true},
+
+        };
+    }
+}
