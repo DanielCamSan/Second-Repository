e370da2f2a821251a00348e9b174b9c06da0eca3
jmss <juanma.soruco@gmail.com>
2025-09-26 16:41:31 -0400

    fix: coorection on model

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -10,14 +10,14 @@ namespace FirstExam.Models
     {
         [Required] public Guid Id { get; set; }
         [Required] public Guid PetId { get; set; }
-        [Required] public DateOnly ScheduledAt { get; set; } // fecha y hora agendada  
+        [Required] public DateTime ScheduledAt { get; set; } // fecha y hora agendada  
         [Required] public String Reason { get; set; }// vacunación, control, cirugía, etc. 
         [Required] public String Status { get; set; } = "scheduled";// scheduled | completed | canceled | no_show 
         public String? Notes { get; set; }
     }
     public record CreateAppointmentDto
     {
-        [Required] public DateOnly ScheduledAt { get; init; } // fecha y hora agendada  
+        [Required] public DateTime ScheduledAt { get; init; } // fecha y hora agendada  
         [Required] public String Reason { get; init; }// vacunación, control, cirugía, etc. 
         [Required] public String Status { get; init; } = "scheduled";// scheduled | completed | canceled | no_show 
         public String? Notes { get; init; }
