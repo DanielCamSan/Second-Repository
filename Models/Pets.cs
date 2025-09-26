04ec247916cd0e97a3f0b36265249f3640ac91af
unknown <leonardo.camacho@ucb.edu.bo>
2025-09-26 16:45:35 -0400

    Feat:Create PetsDto

--- Models/Pets.cs
+++ Models/Pets.cs
@@ -1,6 +1,45 @@
-﻿namespace FirstExam.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstExam.Models
 {
     public class Pets
     {
+        public Guid Id { get; set; }
+        public Guid OwnerId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public string Species { get; set; } = string.Empty;
+
+        public string Breed { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; }
+        
+        public string Sex { get; set; } = string.Empty;
+
+        public decimal? WeightKg { get; set; }
+        }
+    public record createPetsDTO
+    {
+        public Guid OwnerId { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        [RegularExpression("^(dog | cat | bird | reptile | other)$")]
+        public string Species { get; set; } = string.Empty;
+        public string Breed { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; }
+
+
+        [RegularExpression("^(M | F )$")]
+        public string sex { get; set; } = string.Empty;
+
+        public decimal? WeightKg { get; set; }
     }
 }
+/* Guid Id, 
+    Guid OwnerId, 
+    string Name, 
+    string Species // dog | cat | bird | reptile | other 
+    string Breed, 
+    DateTime BirthDate, 
+    string sex, 
+    decimal? WeightKg ) 
+*/
