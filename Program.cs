7093495ec126273014d7ca50b69d43cafd296821
DanielCamSan <daniel.camacho.santacruz.5c@gmail.com>
2025-09-27 16:24:36 -0400

    comentarios evaluacion

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("MyPolicyCors", policy =>
     {
+        //cors era para todos los origenes
         policy.WithOrigins("http://127.0.0.1:5500").AllowAnyMethod().AllowAnyHeader();
     });
 });
